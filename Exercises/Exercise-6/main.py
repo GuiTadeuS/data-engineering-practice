f16c9af9e2620af48002062780e58fb959ad10ad
GuiTadeuS <tadeu.guilherme2002@gmail.com>
2025-02-09 21:40:18 -0300

    fix: add functional based report output

--- Exercises/Exercise-6/main.py
+++ Exercises/Exercise-6/main.py
@@ -1,48 +1,64 @@
 import logging
 from pathlib import Path
 from tempfile import TemporaryDirectory
+from typing import Callable, List
 from zipfile import ZipFile
-from pyspark.sql import SparkSession
-from pyspark.sql.types import (StructType,
-                               StructField,
-                               StringType,
-                               TimestampType)
+from pyspark.sql import SparkSession, DataFrame
 
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s - %(levelname)s - %(message)s")
 
 
-schema = StructType([
-    StructField("trip_id", StringType()),
-    StructField("start_time", TimestampType()),
-    StructField("end_time", TimestampType()),
-    StructField("bikeid", StringType()),
-    StructField("tripduration", StringType()),
-    StructField("from_station_id", StringType()),
-    StructField("from_station_name", StringType()),
-    StructField("to_station_id", StringType()),
-    StructField("to_station_name", StringType()),
-    StructField("usertype", StringType()),
-    StructField("gender", StringType()),
-    StructField("birthyear", StringType()),
-])
+REPORTS_DIRECTORY: Path = str(Path(__file__).parent / "reports")
 
 
-def process_csv(zip_file: ZipFile, filename, spark: SparkSession):
+def get_df_csv_from_zip(zip_file: ZipFile, filename, spark: SparkSession):
     with TemporaryDirectory() as tempdir:
         csv_path = zip_file.extract(filename, path=tempdir)
         try:
-            df = spark.read.csv(csv_path, header=True, schema=schema)
+            df = spark.read.csv(csv_path, header=True, inferSchema=True)
+            df.cache()
+            df.count()
             logging.info("Successfully created DataFrame")
             return df
         except Exception as e:
             logging.error(f"Failed to create DataFrame with {csv_path}: {e}")
 
 
+def process_dataframes(dfs: List[DataFrame]):
+    output_report(dfs, average_trip_duration)
+
+
+def output_report(dfs: List[DataFrame], processment_function: Callable):
+    result_df: DataFrame = processment_function(dfs)
+    if result_df:
+        try:
+            output_path = f"{REPORTS_DIRECTORY}\\{processment_function.__name__}.csv"
+            logging.info(f"Attempting to write output to: {output_path}")
+
+            result_df.coalesce(1).write \
+                .format("csv") \
+                .mode('overwrite') \
+                .option("header", True) \
+                .save(output_path)
+
+            logging.info(f"Successfully saved output to: {output_path}")
+        except Exception as e:
+            logging.error(f"Failed to process and generate output: {e}")
+            logging.error(f"Attempted to access file at: {output_path}")
+    else:
+        logging.error("The resulting DataFrame is empty or invalid.")
+
+
+def average_trip_duration(dfs: List[DataFrame]):
+    return dfs[0]
+
+
 def main():
     logging.info("Start")
-    spark = SparkSession \
+
+    spark: SparkSession = SparkSession \
         .builder \
         .appName("Exercise6") \
         .enableHiveSupport() \
@@ -51,12 +67,18 @@ def main():
     data_directory = Path(__file__).parent / "data"
     zips = [str(file) for file in data_directory.rglob("*.zip")]
 
+    dfs: List[DataFrame] = []
     for zip_file in zips:
         with ZipFile(zip_file) as zip_file:
             zipinfo = zip_file.infolist()
             for file_info in zipinfo:
-                if file_info.filename.endswith(".csv"):
-                    process_csv(zip_file, file_info.filename, spark)
+                if file_info.filename.endswith(".csv") and "__MACOSX" not in file_info.filename:
+                    dfs.append(get_df_csv_from_zip(zip_file,
+                                                   file_info.filename,
+                                                   spark))
+    if dfs:
+        process_dataframes(dfs)
+
     logging.info("End")
 
 
